17299541ac507eb9e8fc1fb1d51e5b077df7a373
Mkranj <mkranjcev6@gmail.com>
2023-02-12 00:15:47 +0100

    Test reading footnotes from .docx
    When running pytest, it should xpass.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -40,3 +40,33 @@ def test_read_analyze_text():
     # Dealing with Croatian characters errors: Save the .txt file with ANSI encoding!
     
     assert narrower_citations.citations == expected_output
+    
+@pytest.mark.xfail(reason = "VSCode Pytest filepath issue. In terminal, this should xpass")
+def test_main_text_and_docx_footnotes_analyzed():
+    text = PapersCited.read_document("tests/document_footnotes.docx")
+    
+    # Get all types of citations
+    solo_authors = PapersCited.get_matches_solo_author(text, drop_excluded_phrases = True)
+    two_authors = PapersCited.get_matches_two_authors(text, drop_excluded_phrases = True)
+    three_authors = PapersCited.get_matches_three_authors(text, drop_excluded_phrases = True)
+    author_et_al = PapersCited.get_matches_author_et_al(text, drop_excluded_phrases = True)
+    two_surnames = PapersCited.get_matches_two_surnames(text, drop_excluded_phrases = True)
+    two_surnames_et_al = PapersCited.get_matches_two_surnames_et_al(text, drop_excluded_phrases = True)
+        
+    solo_authors.delete_clones_of_citations(two_authors)
+    
+    narrower_citations = PapersCited.CitationType(solo_authors.citations + 
+                                      two_authors.citations +
+                                      author_et_al.citations)
+    
+    wider_citations = PapersCited.CitationType(three_authors.citations + 
+                                   two_surnames.citations +
+                                   two_surnames_et_al.citations)
+
+    narrower_citations.cleanup()
+    wider_citations.cleanup(allow_commas = False) 
+    
+    expected_output = ["FootnoteAuthor 1111", "FootnoteAuthorTwo 2222",  "RegularAuthor 2010"]
+      
+    assert narrower_citations.citations == expected_output
+  
