72112202b5bab000ae1af5ad7f0397afc6750f6d
Mkranj <mkranjcev6@gmail.com>
2023-07-14 20:24:02 +0200

    Configure row and colspans

--- window_ui.py
+++ window_ui.py
@@ -3,35 +3,35 @@ from tkinter import filedialog
 from tkinter import messagebox
 
 main_window = tk.Tk()
+main_window.columnconfigure(0, weight = 0, minsize = 50)
+main_window.columnconfigure(1, weight = 1, minsize = 100)
+main_window.rowconfigure(1, minsize = 400)
 
 btn_choose = tk.Button(master = main_window,
                         text = "Choose document")
 
-btn_choose.grid(row = 0, column = 0)
+btn_choose.grid(row = 0, column = 0, sticky = "NW")
 
-main_window.columnconfigure(0, weight = 0, minsize = 50)
-main_window.columnconfigure(1, weight = 1, minsize = 100)
 
 lbl_current_file = tk.Label(master = main_window,
                             text = "path/to/file", bg = "blue")
 
-lbl_current_file.grid(row = 0, column = 2)
+lbl_current_file.grid(row = 0, column = 1, columnspan = 3, sticky = "WE")
 
 lbl_results = tk.Label(master = main_window,
                        text = "Results will be shown here...")
 
-lbl_results.grid(row = 1, column = 1)
-main_window.rowconfigure(1, minsize = 400)
+lbl_results.grid(row = 1, column = 0, sticky = "nwse", columnspan = 4)
 
 btn_save_xlsx = tk.Button(master = main_window,
                           text = "Save as .xlsx")
 
-btn_save_xlsx.grid(row = 2, column = 2)
+btn_save_xlsx.grid(row = 2, column = 2, sticky = "SE")
 
 btn_save_txt = tk.Button(master = main_window,
                           text = "Save as .txt")
 
-btn_save_txt.grid(row = 2, column = 3)
+btn_save_txt.grid(row = 2, column = 3, sticky = "SE")
 
 main_window.mainloop()
 
