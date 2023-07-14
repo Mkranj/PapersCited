4d1e2b553444489315de94538b589c6945bd4f13
Mkranj <mkranjcev6@gmail.com>
2023-07-14 20:33:09 +0200

    Minsize so no overlap

--- window_ui.py
+++ window_ui.py
@@ -5,8 +5,11 @@ from tkinter import messagebox
 main_window = tk.Tk()
 main_window.columnconfigure(0, weight = 0, minsize = 50)
 main_window.columnconfigure(1, weight = 1, minsize = 100)
-main_window.rowconfigure(1, minsize = 400)
-main_window.rowconfigure(2, weight = 1, minsize = 50)
+main_window.rowconfigure(0, weight = 0, minsize = 50)
+main_window.rowconfigure(1, weight = 2, minsize = 400)
+main_window.rowconfigure(2, weight = 0, minsize = 50)
+
+main_window.minsize(750, 500)
 
 btn_choose = tk.Button(master = main_window,
                         text = "Choose document")
@@ -17,10 +20,11 @@ btn_choose.grid(row = 0, column = 0, sticky = "NW")
 lbl_current_file = tk.Label(master = main_window,
                             text = "path/to/file", bg = "blue")
 
-lbl_current_file.grid(row = 0, column = 1, columnspan = 3, sticky = "WE")
+lbl_current_file.grid(row = 0, column = 1, columnspan = 3, sticky = "NWE")
 
 lbl_results = tk.Label(master = main_window,
-                       text = "Results will be shown here...")
+                       text = "Results will be shown here...",
+                       bg = "white")
 
 lbl_results.grid(row = 1, column = 0, sticky = "NWSE", columnspan = 4)
 
