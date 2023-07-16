a1ab8e214e001987e9805a5a551364c1de967c79
Mkranj <mkranjcev6@gmail.com>
2023-07-16 11:27:10 +0200

    PapersCited calls the window app

--- PapersCited/PapersCited.py
+++ PapersCited/PapersCited.py
@@ -1,3 +1,5 @@
+from windowUI import main_window
+
 # -*- coding: utf-8 -*-
 version = "v.1.2.3"
 
@@ -9,38 +11,7 @@ if __name__ == "__main__":
 # MAIN ----
 
 def main():
-    print("\nChoose the file you want to find citations in.")
-    filename = get_file()
-    check_file(filename)
-    document = read_document(filename)
-    
-    # Get all types of citations
-    solo_authors = get_matches_solo_author(document, drop_excluded_phrases = True)
-    two_authors = get_matches_two_authors(document, drop_excluded_phrases = True)
-    three_authors = get_matches_three_authors(document, drop_excluded_phrases = True)
-    author_et_al = get_matches_author_et_al(document, drop_excluded_phrases = True)
-    two_surnames = get_matches_two_surnames(document, drop_excluded_phrases = True)
-    two_surnames_et_al = get_matches_two_surnames_et_al(document, drop_excluded_phrases = True)
-        
-    solo_authors.delete_clones_of_citations(two_authors)
-    
-    narrower_citations = CitationType(solo_authors.citations + 
-                                      two_authors.citations +
-                                      author_et_al.citations)
-    
-    wider_citations = CitationType(three_authors.citations + 
-                                   two_surnames.citations +
-                                   two_surnames_et_al.citations)
-    
-    narrower_citations.cleanup()
-    wider_citations.cleanup(allow_commas = False) # False prevents lots of duplication
-    write_excel(filename, narrower_citations, wider_citations)
-    preview_citations(narrower_citations, wider_citations)
+   main_window.mainloop()
 
 if __name__ == "__main__":
-    continue_processing_files = True
-    while continue_processing_files:
-        main()
-        continue_processing_files = dialog_process_another_file()
-        if continue_processing_files:
-            print("\n-------------------\n")
+    main()
