8b8f5a3040b8dcd3589c2c171e051db2d306fa83
Mkranj <mkranjcev6@gmail.com>
2023-10-01 12:49:58 +0200

    Update appdata test to not take widget args

--- PapersCited/tests/test_appdata.py
+++ PapersCited/tests/test_appdata.py
@@ -9,15 +9,15 @@ def test_checking_for_empty_citation_list():
     
     appdata = AppData("", "")
     
-    appdata.set_new_results_citations([narrow_empty, wide_empty], list_affected_wg=[])
+    appdata.set_new_results_citations([narrow_empty, wide_empty])
     assert appdata.any_citations_recorded() == False
     
-    appdata.set_new_results_citations([narrow_full, wide_empty], list_affected_wg=[])
+    appdata.set_new_results_citations([narrow_full, wide_empty])
     assert appdata.any_citations_recorded() == True
     
-    appdata.set_new_results_citations([narrow_empty, wide_full], list_affected_wg=[])
+    appdata.set_new_results_citations([narrow_empty, wide_full])
     assert appdata.any_citations_recorded() == True
     
-    appdata.set_new_results_citations([narrow_full, wide_full], list_affected_wg=[])
+    appdata.set_new_results_citations([narrow_full, wide_full])
     assert appdata.any_citations_recorded() == True
     
