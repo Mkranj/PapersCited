afa4a614847b24fa3ed9ab61b707e0be82103f51
Mkranj <mkranjcev6@gmail.com>
2023-10-05 19:56:31 +0200

    Add test for wrapper fn
    Consolidates the process of finding, cleaning and removing duplicate citations

--- tests/test_detecting_authors.py
+++ tests/test_detecting_authors.py
@@ -1,4 +1,5 @@
 import citationAnalysis.citationAnalysis as ca
+from UI.fileManipulation import find_citations
 
 # The \ indicates code continues in the next line
 # When testing individual detection functions, they SHOULD catch extra characters
@@ -172,3 +173,13 @@ def test_surnames_apostrophe_s_recognised():
     matches.cleanup()
     assert matches.citations == ["O'Samuel 1999", "O'Sullivan 2000"]
  
+def test_find_citations_wrapper():
+    text = "One and two (1212) often collaborate. This is well documented (Aesop and Berry, 1999). Aasimov (1999) had a revelation (Bassimov, 2000). Long Surname (2020) found his match."
+    
+    # Specifying sought-after matches. Berry, 1999 is a side-effect not considered harmful.
+    expected_narrow = ["Aasimov 1999", "Bassimov 2000", "Aesop & Berry 1999", "One & two 1212"]
+    expected_wider = ["Long Surname 2020"]
+    
+    found_citations = find_citations(text)
+    assert all([citation in found_citations[0].citations for citation in expected_narrow])
+    assert all([citation in found_citations[1].citations for citation in expected_wider])
